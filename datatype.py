f5bf485d9add515786da10c37800624d997a0335
Amjad Sharaf <amjadsharaf406@gmail.com>
2026-01-02 05:52:09 -0800

    data type

---
+++ datatype.py
@@ -0,0 +1,18 @@
+#-------------------------------------
+#datatype in python
+#-------------------------------------
+
+
+
+#-------------------------------------
+
+print (type(10))        #int => integer
+print (type(10.5))      #float => decimal number
+print (type("hello"))   #str => string
+print (type(True))      #bool => boolean
+print (type([1,2,3]))  #list =>List
+print (type((1,2,3)))  #tuple => tuple
+print (type({"one" : 1 , "two" : 2 , "three" : 3} )) #dict => dictionary
+print (type({1,2,3}))  #set => set
+print (type(None))      #NoneType => null value
+#-------------------------------------

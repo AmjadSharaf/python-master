7ec2666e437e5fb15aa0bc6ad033fb21c352b17e
Amjad Sharaf <amjadsharaf406@gmail.com>
2026-01-02 05:32:16 -0800

    comments

---
+++ 1.py
@@ -0,0 +1,6 @@
+print ("i love python")
+#coments
+#--------------------------------------
+# This is a single line comment
+#create the first python program
+#--------------------------------------

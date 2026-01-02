4ffac7e666eeec9ee659e8345aef95f9308b71be
Amjad Sharaf <amjadsharaf406@gmail.com>
2026-01-02 06:18:13 -0800

    Variable

---
+++ variable.py
@@ -0,0 +1,55 @@
+#--------------------------------------
+# syntax => [variable_name]  [Assigmmant Operator] [value]
+#
+# [1] can stat with (a-z) , (A-Z) , (_) underscore
+# [2] can contain num or spacial charcters but not at the begining
+# [3] can include (0-9) , (_) underscore
+# [4] cannot spacial charcters like (! , @ , # , $ , % , ^ , & , * , ( ) , - , + , = , { } , [ ] , | , \ , : , ; , " ' < > , . , ? , / )
+# [5] case sensitive (myVar , Myvar , MYVAR) are different
+#--------------------------------------
+
+name = "Amjad" #single word variable
+my_name = "Amjad Sharaf" #two words variable with underscore => snake case
+myName = "Amjad Sharaf" #two words variable with camel case
+
+
+
+print(myName)
+print(name)
+print(my_name)
+#--------------------------------------
+
+
+
+
+#-------------------------------------
+# source code => original code write it in computer language
+# Translator => a program that convert source code to machine code
+# compilation => translate code before run time 
+#Run time => the time when the program is running
+#-------------------------------------
+
+
+
+age = 25  
+age = 22
+
+
+print(age) #  => 22
+#--------------------------------------
+
+# forbidding Words 
+# int string float bool list tuple dict set None class
+# def if else elif while for try except finally with as return
+# import from global nonlocal lambda pass break continue yield
+# raise assert del and or not is in
+#-------------------------------------
+print("Keywords")
+
+
+
+
+a ,b , c = 1 , 2 , 3
+print(a)
+print(b)   
+print(c)
